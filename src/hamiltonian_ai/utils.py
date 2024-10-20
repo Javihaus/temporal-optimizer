984c5e0f403b571546eb1368c634a17699facf35
Javier Marin <javier@jmarin.info>
2024-10-20 13:58:58 +0200

    Update utils.py

--- src/hamiltonian_ai/utils.py
+++ src/hamiltonian_ai/utils.py
@@ -15,7 +15,7 @@ def evaluate_model(model, dataloader, device):
             all_labels.extend(labels.cpu().numpy())
 
     accuracy = accuracy_score(all_labels, all_preds)
-    precision, recall, f1, _ = precision_recall_fscore_support(all_labels, all_preds, average='weighted')
+    precision, recall, f1, _ = precision_recall_fscore_support(all_labels, all_preds, average='weighted', zero_division=1)
     auc = roc_auc_score(all_labels, all_preds)
 
     return accuracy, precision, recall, f1, auc
