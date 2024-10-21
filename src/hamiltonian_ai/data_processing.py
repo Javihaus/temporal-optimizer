a54abd57105ca6fb3ee112b4bac7821238b30911
Javier Marin <javier@jmarin.info>
2024-10-21 12:22:00 +0200

    Update data_processing.py

--- src/hamiltonian_ai/data_processing.py
+++ src/hamiltonian_ai/data_processing.py
@@ -1,5 +1,5 @@
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE
