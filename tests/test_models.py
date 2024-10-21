a00b18ca69879ef08b34f792e0108eb7179d3611
Javier Marin <javier@jmarin.info>
2024-10-21 18:13:29 +0200

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -17,24 +17,22 @@ def test_model_initialization(model):
     assert model.layers[1].out_features == 32
     assert model.layers[2].in_features == 32
     assert model.layers[2].out_features == 2
-
+    
 
 def test_model_forward(model):
     x = torch.randn(5, 10)
     output = model(x)
-
     assert output.shape == (5, 2)
     assert output.requires_grad
+
 
 
 def test_model_backward(model):
     x = torch.randn(5, 10)
     y = torch.randint(0, 2, (5,))
-    
     output = model(x)
     loss = torch.nn.functional.cross_entropy(output, y)
     loss.backward()
-
     for param in model.parameters():
         assert param.grad is not None
 
@@ -42,28 +40,22 @@ def test_model_backward(model):
 def test_model_with_different_activation(model):
     model_relu = HamiltonianNN(input_dim=10, hidden_dims=[64, 32], activation='relu')
     x = torch.randn(5, 10)
-
     output_leaky = model(x)
     output_relu = model_relu(x)
-
     assert not torch.allclose(output_leaky, output_relu)
 
 
 def test_model_dropout():
     model_with_dropout = HamiltonianNN(input_dim=10, hidden_dims=[64, 32], dropout_rate=0.5)
     x = torch.randn(5, 10)
-
     # Set model to eval mode
     model_with_dropout.eval()
     with torch.no_grad():
         output1 = model_with_dropout(x)
         output2 = model_with_dropout(x)
-
     assert torch.allclose(output1, output2)
-
     # Set model to train mode
     model_with_dropout.train()
     output1 = model_with_dropout(x)
     output2 = model_with_dropout(x)
-
     assert not torch.allclose(output1, output2)
